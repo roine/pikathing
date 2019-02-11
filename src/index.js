a9190a7941dd33df3f35278022e82d0fa01c0b2c
roine <feunetre@hotmail.com>
2019-02-11 11:10:19 +1100

    Make is PWA

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import './main.css';
+import { Elm } from './Main.elm';
+import registerServiceWorker from './registerServiceWorker';
+const key = "app"
+const storage = localStorage.getItem(key)
+
+const app = Elm.Main.init({
+  node: document.getElementById("elm-node"),
+  flags: storage
+})
+
+app.ports.save.subscribe(function (data) {
+  localStorage.setItem(key, data)
+})
+
+window.debug = JSON.parse(localStorage.app)
+
+app.ports.export_.subscribe(function () {
+  const data = localStorage.getItem(key)
+  var dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(data)
+  var downloadAnchorNode = document.createElement('a')
+  downloadAnchorNode.setAttribute("href", dataStr)
+  downloadAnchorNode.setAttribute("download", "backup.json")
+  document.body.appendChild(downloadAnchorNode) // required for firefox
+  downloadAnchorNode.click()
+  downloadAnchorNode.remove()
+})
+
+registerServiceWorker();
+
